81014b82a2885bf6c693738f2c975a4a04639e3c
Aadarsh Pandit <aadarshapandit@gmail.com>
2026-03-02 01:02:18 +0545

    Add beverages project to sitemap

--- portfolio/app/sitemap.ts
+++ portfolio/app/sitemap.ts
@@ -36,6 +36,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
             changeFrequency: 'monthly',
             priority: 0.5,
         },
+        {
+            url: 'https://aadarshapandit.com.np/projects/beverages',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
         ...blogEntries,
     ]
 }
